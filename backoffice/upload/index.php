197494c1db88f8b2fbebf0bcc5dd69cc5d615ca6
LfksBen <benjaminbraud@hotmail.fr>
2014-11-28 01:06:49 +0100

    Correction

--- backoffice/upload/index.php
+++ backoffice/upload/index.php
@@ -21,15 +21,24 @@ if(isset($_POST["submit"])) {
 
 	$imgUploaded = $_FILES["fileToUpload"]["tmp_name"];
 	$imgDest = "../images/".$_FILES["fileToUpload"]["name"];
+	echo filesize($imgUploaded);
+	// treat only file under 8mo
+	if (filesize($imgUploaded) > 8388608) {
+		echo "test";
+		// copy image without resizing
+		move_uploaded_file($imgUploaded, "../images/".pathinfo($image)['filename']."_desktop.".pathinfo($image)['extension']);
+		move_uploaded_file($imgUploaded, "../images/".pathinfo($image)['filename']."_mobile.".pathinfo($image)['extension']);
+		return;
+	}
 	
 	// Check if image file is a actual image or fake image
     $check = getimagesize($imgUploaded);
-    if($check !== false) {
+    if($check !== false && filesize($imgUploaded) <= 8388608) {
        move_uploaded_file($imgUploaded, $imgDest);
-		
-       $ext = pathinfo($imgDest)['extension'];
-       $destkopImg = pathinfo($imgDest)['dirname']."/".pathinfo($imgDest)['filename']."_desktop.".$ext;
-       $mobileImg = pathinfo($imgDest)['dirname']."/".pathinfo($imgDest)['filename']."_mobile.".$ext;
+	   
+		$ext = pathinfo($imgDest)['extension'];
+		$destkopImg = pathinfo($imgDest)['dirname']."/".pathinfo($imgDest)['filename']."_desktop.".$ext;
+		$mobileImg = pathinfo($imgDest)['dirname']."/".pathinfo($imgDest)['filename']."_mobile.".$ext;
        
 		// resize the image to match a width of 900px (desktop)
 	   $resize->resizeWithProportion($imgDest, $destkopImg, 900, $ext);
@@ -45,7 +54,7 @@ if(isset($_POST["submit"])) {
 	  unlink($imgDest);
 	   
     } else {
-        echo "File is not an image.";
+        echo "<strong>File is not an image</strong>";
     }
 }
 ?>
